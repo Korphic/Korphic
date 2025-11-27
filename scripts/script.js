f0dd81d7d3784eb72bdc277869ece6ab7fe550a1
Korphic <kierluna321@gmail.com>
2025-11-27 18:32:47 +0800

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function () {
+    const preloader = document.getElementById("preloader");
+    preloader.classList.add("fade-out");
+
+    setTimeout(() => {
+        preloader.style.display = "none";
+    }, 1000);
+});
